ce35487c3d46e69e05a33a32c36efdf4e3ad80d2
Marco Cerda <marco.cerda@mercadolibre.cl>
2026-06-25 10:31:31 -0400

    feat: config web

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.MATCH_ALPHA_CONFIG = {
   API_BASE_URL: 'https://matchalpha.bodegafresh.workers.dev/api/v1',
   DEFAULT_SEASON: 'wc2026',
-  KEY_STORAGE: 'match_alpha_web_key',
+  KEY_STORAGE: 'poolteam2026',
   AUTO_REFRESH_MS: 30000
 };
