9d6a9ef6e0d299f49c7e42fb25002825cf3dddd7
Marco Cerda <marco.cerda@mercadolibre.cl>
2026-06-26 01:03:18 -0400

    feat: added ux

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.MATCH_ALPHA_CONFIG = {
-  API_BASE_URL: 'http://localhost:8000/api/v1',
+  API_BASE_URL: 'https://match-alpha.onrender.com/api/v1',
   DEFAULT_SEASON: 'wc2026',
   KEY_STORAGE: 'poolteam2026',
   AUTO_REFRESH_MS: 30000
